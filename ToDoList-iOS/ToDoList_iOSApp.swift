6c44e50a01ee8a1a1bb206eef8e38805df61563a
DGalbacheva <dglbacheva@gmail.com>
2024-03-21 11:04:34 +0300

    Initial Commit

---
+++ ToDoList-iOS/ToDoList_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_iOSApp.swift
+//  ToDoList-iOS
+//
+//  Created by Doroteya Galbacheva on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
