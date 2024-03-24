7a3e68504021d6fc5794cad6902e2f3fae2bf3bc
DGalbacheva <dglbacheva@gmail.com>
2024-03-24 18:33:31 +0300

    Created HeaderView

---
+++ ToDoList-iOS/Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  ToDoList-iOS
+//
+//  Created by Doroteya Galbacheva on 24.03.2024.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
