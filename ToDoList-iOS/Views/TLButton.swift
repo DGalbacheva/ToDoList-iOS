386af5e12d9d60b185e072473f25723224e9acc8
DGalbacheva <dglbacheva@gmail.com>
2024-03-30 12:25:12 +0300

    Created Form for RegisterView, FullName, Email, Password.

---
+++ ToDoList-iOS/Views/TLButton.swift
@@ -0,0 +1,18 @@
+//
+//  TLButton.swift
+//  ToDoList-iOS
+//
+//  Created by Doroteya Galbacheva on 30.03.2024.
+//
+
+import SwiftUI
+
+struct TLButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TLButton()
+}
